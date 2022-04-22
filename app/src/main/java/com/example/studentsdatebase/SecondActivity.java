bfa7f369dcd7b1dd840941f3ee87a27f0c4869f4
Skitcc <ivan.g.ermakov@yandex.ru>
2022-04-22 17:38:37 +0300

    Значение с textView на Button, не дописал адаптер

--- app/src/main/java/com/example/studentsdatebase/SecondActivity.java
+++ app/src/main/java/com/example/studentsdatebase/SecondActivity.java
@@ -4,24 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("message", "text");
-        Button btn = findViewById(R.id.AddStudent);
+        setContentView(R.layout.activity_second);
+        Button btn = findViewById(R.id.button2);
+        TextView txt = findViewById(R.id.group_name);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(SecondActivity.this, ChooseGroup.class);
+                intent.putExtra("group_name", txt.getText().toString());
                 startActivity(intent);
-
             }
         });
     }
